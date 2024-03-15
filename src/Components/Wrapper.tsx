d68956efb5f2ab79d9693eaf5ea26e2748fccee5
Your Name <y.y.oleksander@gmail.com>
2024-03-15 19:42:48 +0100

    1

--- src/Components/Wrapper.tsx
+++ src/Components/Wrapper.tsx
@@ -1,7 +1,7 @@
 // import React, { useRef, useEffect } from "react";
 import { ReactNode } from "react";
 import GameDetails from "./GameDetails";
-import handleSwipeEvent from "../engine/events/handleSwipeEvent";
+// import handleSwipeEvent from "../engine/events/handleSwipeEvent";
 
 function Wrapper({ children }: { children: ReactNode }) {
   // const wrapperRef = useRef(null);
@@ -13,16 +13,23 @@ function Wrapper({ children }: { children: ReactNode }) {
   //   }
   // }, []);
 
-  const handleTouch = (e: React.TouchEvent<HTMLDivElement>) => {
-    handleSwipeEvent(e); // Вызываем пользовательскую функцию обработки свайпа
+  // const handleTouch = (e: React.TouchEvent<HTMLDivElement>) => {
+  //   handleSwipeEvent(e); // Вызываем пользовательскую функцию обработки свайпа
+  // };
+
+  const touchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+    console.log(e);
+    // const firstTouch = e.touches[0];
+    // x1 = firstTouch.clientX;
+    // y1 = firstTouch.clientY;
+  };
+
+  const touchEnd = (e: React.TouchEvent<HTMLDivElement>) => {
+    console.log(e);
   };
 
   return (
-    <div
-      className="wrapper"
-      onTouchStart={handleTouch}
-      onTouchMove={handleTouch}
-    >
+    <div className="wrapper" onTouchStart={touchStart} onTouchEnd={touchEnd}>
       <GameDetails />
       {children}
     </div>
